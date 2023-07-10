ba95f0e8c9b82dd0dff55cc185e6f3b2ccd3fc77
Dawid Migdał <dejf026@gmail.com>
2023-07-10 17:18:24 +0200

    done

--- SeleniumCsharpDM/OrderSkipAttribute.cs
+++ SeleniumCsharpDM/OrderSkipAttribute.cs
@@ -11,6 +11,7 @@ using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
 
 namespace SeleniumCsharpDM
 {
@@ -18,17 +19,19 @@ namespace SeleniumCsharpDM
     public class OrderSkipAttribute
     {
 
-        [Test, /*Order(2),*/ Category("OrderSkipAttribute")]
+        [Test, Order(2), Category("OrderSkipAttribute")]
         public void TestMethod1()
         {
-            //Assert.Ignore("Defect 12345");
+            Assert.Ignore("Defect 12345");
+
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://www.facebook.com/";
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
             driver.Close();
         }
-        [Test, /*Order(1),*/ Category("OrderSkipAttribute")]
+        [Test, Order(1), Category("OrderSkipAttribute")]
+        
         public void TestMethod2()
         {
             IWebDriver driver = new FirefoxDriver();
@@ -37,10 +40,10 @@ namespace SeleniumCsharpDM
             emailTextField.SendKeys("Selenium C#");
             driver.Close();
         }
-        [Test, /*Order(0),*/ Category("OrderSkipAttribute")]
+        [Test, Order(0), Category("OrderSkipAttribute")]
         public void TestMethod3()
         {
-            IWebDriver driver = new InternetExplorerDriver();
+            IWebDriver driver = new EdgeDriver();
             driver.Url = "https://www.facebook.com/";
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
